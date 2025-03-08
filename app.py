b7e5125aed504a0fb6bea387622209b9faa66c15
Rah1131 <149700190+Rah1131@users.noreply.github.com>
2025-03-08 18:34:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ n = input('Enter "C" to create a QR Code (or) "U" to Upload QR code path: ')
 
 if n.lower() == "c":
     # Generate a QR code from user input
-    n1 = input("Enter the string to convert into a QR Code: ")
+    n1 = input("Enter the string/URL to convert into a QR Code: ")
     qr = pyqrcode.create(n1)  # Create QR code
     qr.png("myQrCode.png", scale=8)  # Save QR code as PNG with a scale of 8
     print("QR Code saved as myQrCode.png")  # Notify the user
 
 elif n.lower() == "u":
     # Decode a QR code from an uploaded image
-    image_path = input("Enter the path of the QR code file: ")
+    image_path = input("Enter the path of the QR code image: ")
     try:
         img = PIL.Image.open(image_path)  # Open the image
         decoded_data = decode(img)  # Decode the QR code
